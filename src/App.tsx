a4b7d46afbd7864fd7a2e298e507cd1132a96937
emarson.halcyondigital <elbancud@gmail.com>
2022-08-01 11:02:30 +0800

    data populated: project on halt: TODO: Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import { useEffect } from 'react';
 import * as THREE from 'three';
 import './App.css';
+import { socials } from './data/contents';
 
+interface ISocials {
+  label: string;
+  link: string;
+  icon: string;
+}
 function App() {
   useEffect(() => {
     const scene = new THREE.Scene();
   }, []);
   return (
     <div className='App'>
-      <canvas id='myThreeJsCanvas' />
+      {/* <canvas id='myThreeJsCanvas' />
+       */}
+      <ul>
+        {socials &&
+          socials.map((social: ISocials) => {
+            return <li>{social.label}</li>;
+          })}
+      </ul>
     </div>
   );
 }
---
+++ src/data/contents.ts
@@ -0,0 +1,25 @@
+export const socials = [
+  {
+    label: 'Github',
+    link: '#',
+    icon: '',
+  },
+  {
+    label: 'Linked In',
+    link: '#',
+    icon: '',
+  },
+];
+
+export const projects = [
+  {
+    name: 'VJM Consultancy Property',
+    usecase: [{}],
+    logo: './src',
+  },
+  {
+    name: 'HESOYAM: Operation Management System',
+    link: '#',
+    icon: '',
+  },
+];
