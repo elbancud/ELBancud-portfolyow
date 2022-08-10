35e6efcdc1e4dc129f059800d6e399bab47558e5
emarson.halcyondigital <elbancud@gmail.com>
2022-08-10 08:14:22 +0800

    chore: client testimonies

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,12 @@
+import ClientTestimonies from '@/components/layouts/sections/client-testimonies';
 import Header from './layouts/Header';
-
 function index() {
   return (
     <div>
       <div className='w-100'>
         {/* <NavBar /> */}
         <Header />
+        <ClientTestimonies />
         {/* <Footer /> */}
       </div>
     </div>
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -13,28 +13,30 @@ function Header() {
   };
   const CircleInterface = (image_source: string) => {
     return (
-      <div className='inline-block rounded-full overflow-hidden h-20 w-20 bg-slate-700'>
+      <div className='inline-block rounded-full overflow-hidden w-16 h:10 sm:h-20 sm:w-20 bg-slate-700'>
         <img src={image_source} alt='interface' />
       </div>
     );
   };
   return (
-    <div className='container text-center m-auto py-8 my-16'>
-      <div className='w-100 grid place-items-center'>
-        {/* <p>Hi there, I'm Emarson Bancud</p> */}
-        <a href='#'>
-          <img className='w-10 h-12' src={Logo} alt='emarson bancud logo' />
-        </a>
-      </div>
-      {/* Built simply by genuinity */}
+    <>
+      <div className='container text-center m-auto py-8 my-16'>
+        <div className='w-100 grid place-items-center'>
+          {/* <p>Hi there, I'm Emarson Bancud</p> */}
+          <a href='#'>
+            <img className='w-10 h-12' src={Logo} alt='emarson bancud logo' />
+          </a>
+        </div>
+        {/* Built simply by genuinity */}
 
-      <h1 className='uppercase '>
-        "Built simply with genuine intention{' '}
-        <span>{CircleInterface(headerImagesLinks[0].url)}</span>, passion{' '}
-        <span>{OvalInterface(headerImagesLinks[0].url)}</span>, and interest.{' '}
-        <span>{OvalInterface(headerImagesLinks[1].url)}</span>"
-      </h1>
-    </div>
+        <h1 className='uppercase '>
+          "Built simply with genuine intention{' '}
+          <span>{CircleInterface(headerImagesLinks[0].url)}</span>, passion{' '}
+          <span>{OvalInterface(headerImagesLinks[0].url)}</span>, and interest.{' '}
+          <span>{OvalInterface(headerImagesLinks[1].url)}</span>"
+        </h1>
+      </div>
+    </>
   );
 }
 
