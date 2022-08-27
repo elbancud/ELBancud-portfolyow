a3ef133b1652ff2d709f38ba1a4cc4441ddc5f12
elbancud <elbancud@gmail.com>
2022-08-27 23:50:59 +0800

    about me progress

--- src/components/layouts/sections/about-me/index.tsx
+++ src/components/layouts/sections/about-me/index.tsx
@@ -1,12 +1,32 @@
 function index() {
   return (
     <>
-      <div className='h-screen container m-auto  text-center py-52'>
-        <div>
-          <h2>Section number 1 </h2>
-          <h3 className='italic '>Lorem ipsum dolor sit amet </h3>
+      <div className='h-screen container m-auto py-52 mt-10 sm:mt-40 px-10'>
+        <div className=''>
+          <p className='leading-normal text-center '>- About me - </p>
+          <h1 className='leading-tight'>
+            I help from the definition of strategy to the achievement of the
+            digital ecosystem
+          </h1>
         </div>
-        <div></div>
+        <div className='sm:flex gap-5 '>
+          <h3 className='py-10'>
+            Since I have always been immersed with full stack development which
+            is responsible for, as I see it, the front-end, back-end, and the
+            entire process of the application.
+          </h3>
+          <h3 className='py-10'>
+            From this, I am able to cater my clients their digital ecosystem
+            which is made up of the target users, 3rd party data services, and
+            respective technologies.
+          </h3>
+        </div>
+
+        <div className='mt-20 sm:mt-96 text-center'>
+          <p className='leading-normal '>- Technologies - </p>
+          <h1>Here's the technologies I am comfortable using.</h1>
+          {/* TODO: Marquee of technologies: Typescript, React JS, Next JS, Vue JS, NodeJS, AdonisJS, MySQL, FireBase, TailwindCSS, MaterialUI, Github, AntDesign, BitBucket */}
+        </div>
       </div>
     </>
   );
