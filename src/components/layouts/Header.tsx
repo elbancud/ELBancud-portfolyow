c6c3c6b0f06ddee2a3c300776c1a410792c27932
elbancud <elbancud@gmail.com>
2022-08-29 16:02:52 +0800

    aboute-me: final layout

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -30,7 +30,7 @@ function Header() {
             <p className='text-slate-300 leading-normal'>
               Hi! I'm Emarson, and all of this are..
             </p>
-            <h1 className='uppercase primary-title-fade'>
+            <h1 className='uppercase primary-title-fade secondary-gradient font-bold'>
               {/* "Built simply with genuine intention{' '}
             <span>{CircleInterface(headerImagesLinks[0].url)}</span>, passion{' '}
             <span>{OvalInterface(headerImagesLinks[0].url)}</span>, and interest.{' '}
@@ -38,7 +38,7 @@ function Header() {
               Built simply with
               {/* Lorem Ipsum */}
             </h1>
-            <h1 className='primary-title pl-0 sm:pl-20 uppercase text-center'>
+            <h1 className='primary-title pl-0 sm:pl-20 uppercase text-center primary-gradient font-bold'>
               Genuine Passion, & Interest
               {/* Lorem Ipsum */}
             </h1>
--- src/components/layouts/sections/about-me/index.tsx
+++ src/components/layouts/sections/about-me/index.tsx
@@ -1,11 +1,25 @@
 function index() {
   return (
     <>
-      <div className='h-screen container m-auto  text-center py-52'>
-        <div>
-          <h2>Section number 1 </h2>
-          <h3 className='italic '>Lorem ipsum dolor sit amet </h3>
+      <div className='h-screen sm:w-100 sm:w-3/4 m-auto sm:pt-96 text-center pt-20'>
+        <div className=''>
+          <p>- About me -</p>
+          <h1 className='leading-none'>
+            FROM <span className='font-bold italic'> DEFINING</span> THE
+            STRATEGY TO
+            <span className='font-bold italic'>ACHIEVING </span> THE DIGITAL
+            ECOSYSTEM
+          </h1>
         </div>
+        <div className='w-100 sm:max-w-4xl m-auto mt-32'>
+          <h3 className='uppercase'>
+            I have always find myself loving the entire process of web
+            interaction. From designing, developing back and front, maintanance,
+            and almost until user engagement. To put it simply, I've always been
+            immersed building websites from the ground up.
+          </h3>
+        </div>
+
         <div></div>
       </div>
     </>
