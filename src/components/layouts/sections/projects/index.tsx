5e1a9195b995b4dc3492361200f40d3f41f7370a
elbancud <elbancud@gmail.com>
2022-09-07 11:25:06 +0800

    fix: animation transform on scroll #projects

--- src/components/layouts/sections/projects/index.tsx
+++ src/components/layouts/sections/projects/index.tsx
@@ -1,35 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import vjm from "@/assets/project-ui/vjm-properties.jpg";
-import hesoyam from "@/assets/project-ui/hesoyam-ch-web.jpg";
-import chooseUp from "@/assets/project-ui/choose-up.jpg";
-import pfc from "@/assets/project-ui/pfc.png";
-import taraenca from "@/assets/project-ui/taraenca.jpg";
+import pfc from '@/assets/project-ui/pfc.png';
 
 function index() {
-  const [scrollXRightTransform, setScrollXRightTransform] = useState<number>();
-  const [scrollXLeftTransform, setScrollXLeftTransform] = useState<number>();
+  const [scrollXRightTransform, setScrollXRightTransform] =
+    useState<number>(-100);
+  const [scrollXLeftTransform, setScrollXLeftTransform] =
+    useState<number>(-100);
 
   useEffect(() => {
     const handleScroll = () => {
-      let scrollYValue = window.scrollY;
-      setScrollXRightTransform(window.scrollY * -0.05);
-      setScrollXLeftTransform(window.scrollY * 0.05);
+      setScrollXRightTransform(window.scrollY * -0.09);
 
-      console.log(window.scrollY);
+      // let scrollYValue =  + ;
+      setScrollXLeftTransform(-300 + window.scrollY * 0.09);
     };
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, [scrollXRightTransform, scrollXLeftTransform]);
 
   const Card = (bgColor, imageSrc, alt) => {
-    const classProperties = `${bgColor} px-10 h-80 grid place-item-center`;
+    const classProperties = `${bgColor} px-10 h-80 grid place-item-center min-w-sm mx-5`;
     return (
       <div className={classProperties}>
         <img
-          className='max-h-md object-contain m-auto'
+          className='max-h-md object-contain max-w-md'
           src={imageSrc}
           alt={alt}
         />
@@ -37,36 +34,61 @@ function index() {
     );
   };
   const GridContainerStyleRight = {
-    transform: `translate(${scrollXRightTransform - 10}px, 0px)`,
+    transform: `translate(${scrollXRightTransform}px, 0px)`,
   };
   const GridContainerStyleLeft = {
     transform: `translate(${scrollXLeftTransform}px, 0px)`,
   };
-  const transLateXClass = `translate-x-${scrollXRightTransform} `;
+  // const transLateXClass = `translate-x-${scrollXRightTransform} `;
   return (
     <>
-      <div className='h-screen w-100 py-96 text-center translate-x '>
+      <div className='h-screen w-100 py-96 text-center '>
         <div className=''>
           <h1 className='leading-none '>Recent works</h1>
         </div>
-        <div style={GridContainerStyleRight}>
+        <div className='block'>
           <div className='relative'>
-            <div className='mt-32 grid grid-cols-4 gap-10 '>
-              <div>{Card("bg-black", vjm, "vjm-comsultancy-property")}</div>
-              <div>{Card("bg-violet-900", hesoyam, "hesoyam-ch")}</div>
-              <div>{Card("bg-orange-900", chooseUp, "choose up design")}</div>
-              <div>{Card("bg-green-600", pfc, "plants for cats")}</div>
+            <div style={GridContainerStyleRight}>
+              <div className='mt-32 flex '>
+                {/* <div>{Card('bg-black', vjm, 'vjm-comsultancy-property')}</div>
+                <div>{Card('bg-violet-900', hesoyam, 'hesoyam-ch')}</div>
+                <div>{Card('bg-orange-900', chooseUp, 'choose up design')}</div> */}
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+              </div>
             </div>
           </div>
         </div>
-        <div style={GridContainerStyleLeft}>
-          <div className='mt-5 grid grid-cols-4 gap-10 '>
-            <div>{Card("bg-black", vjm, "vjm-comsultancy-property")}</div>
-            <div>{Card("bg-violet-900", hesoyam, "hesoyam-ch")}</div>
-            <div>{Card("bg-orange-900", chooseUp, "choose up design")}</div>
-            <div>{Card("bg-green-600", pfc, "plants for cats")}</div>
+        <div className='block'>
+          <div className='relative'>
+            <div style={GridContainerStyleLeft}>
+              <div className='mt-10 flex '>
+                {/* <div>{Card('bg-black', vjm, 'vjm-comsultancy-property')}</div>
+                <div>{Card('bg-violet-900', hesoyam, 'hesoyam-ch')}</div>
+                <div>{Card('bg-orange-900', chooseUp, 'choose up design')}</div> */}
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+              </div>
+            </div>
           </div>
         </div>
+        {/* <div className='block'>
+          <div className='relative'>
+            <div style={GridContainerStyleLeft}>
+              <div className='mt-32 flex'>
+            
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+                <div>{Card('bg-green-600', pfc, 'plants for cats')}</div>
+              </div>
+            </div>
+          </div>
+        </div> */}
         {/* <div className='mt-10 grid grid-cols-4 gap-10 '>
           <div>{Card("bg-black", taraenca, "taraenca")}</div>
         </div> */}
