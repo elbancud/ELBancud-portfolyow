90210d67735e3b80fdbf691df57018c1407f5481
elbancud@gmail <elbancud@gmail.com>
2022-09-27 12:47:24 +0800

    fix: vjm comment

--- src/data/contents.ts
+++ src/data/contents.ts
@@ -96,10 +96,11 @@ export const clientTestimonies = [
     client: 'Jimmy Texon',
     avatar:
       'https://scontent.xx.fbcdn.net/v/t1.15752-9/301641069_2669866883543282_554261295764626237_n.png?_nc_cat=110&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeGjkI5S06hcUlUko2Lm_pAFbdj7U0mdfT9t2PtTSZ19P6sR4Okj89Q0lNsVo875dxTH6DEBAjBGSheNFftOAr6x&_nc_ohc=XTsXPn2G5JwAX_eFlf1&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVKkKBwUmlruXikza5HHlnQSZjW2qUTHaVx0Zuhb5WdBrw&oe=634293A6',
-    comment: 'Son, palagay naman ng bulacan. Wala raw sabi ng encoder ko',
+    comment:
+      'As a client, I am very happy with the outcome of our website that Emarson Bancud provided to us. Encoding our listings is effortless. He provides us with excellent layout results. If there are changes he does it quickly and does it professionally. Also, viewing our listing on clients end is very easy.',
     starCount: 5,
     website: 'VJM Consultancy Property',
   },
 ];
 // Use the generics if an object has multiple nests
-// use extends with generic data type to make sure that the argument passed will be 
+// use extends with generic data type to make sure that the argument passed will be
